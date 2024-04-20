9284da3fce4eeb53a156f14d03af99afbad9455f
Arunav <arunavsutar@gmail.com>
2024-04-20 16:44:34 +0530

    Feat: Added_MVC_Architecture_for_Like_API's

--- src/routes/v1/answers.routes.js
+++ src/routes/v1/answers.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { answerController, commentController } = require('../../controllers');
+const { answerController, commentController, likeController } = require('../../controllers');
 
 const answerRouter = express.Router();
 
 answerRouter.get('/ping', answerController.ping);
 answerRouter.put('/:id', answerController.updateAnswer);
 answerRouter.post('/:id/comments', commentController.commentAAnswer);
+answerRouter.post('/:id/likes', likeController.like);
 
 module.exports = answerRouter;
